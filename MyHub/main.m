49e0924edf3b83d353658376f8d6df16bedceae5
Arcterus <Arcterus@mail.com>
2013-08-09 19:54:17 -0700

    Initial Commit

---
+++ MyHub/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyHub
+//
+//  Created by Alex Lyon on 8/9/13.
+//  Copyright (c) 2013 kRaken Research. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+   @autoreleasepool {
+       return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+   }
+}
---
+++ MyHubTests/MyHubTests.m
@@ -0,0 +1,34 @@
+//
+//  MyHubTests.m
+//  MyHubTests
+//
+//  Created by Alex Lyon on 8/9/13.
+//  Copyright (c) 2013 kRaken Research. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyHubTests : XCTestCase
+
+@end
+
+@implementation MyHubTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
