c7b9219474aaa208b8063241d18d9d6cb3e1ad2e
Arcterus <Arcterus@mail.com>
2014-01-15 13:30:43 -0800

    Add file with dummy client info

---
+++ include/MHClientSecret.h
@@ -0,0 +1,15 @@
+//
+//  MHClientSecret.h
+//  MyHub
+//
+//  Created by Arcterus on 1/14/14.
+//  Copyright (c) 2014 kRaken Research. All rights reserved.
+//
+
+#ifndef MyHub_MHClientSecret_h
+#define MyHub_MHClientSecret_h
+
+#define CLIENT_ID     "1234567890"
+#define CLIENT_SECRET "1234567890abcdef"
+
+#endif
